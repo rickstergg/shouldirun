55eaf63f05c929ac24d804cf2a469f50ff4e90da
Rick <rickzhang@live.ca>
2019-09-05 22:36:44 -0400

    Adding express so we have something to listen to

--- app.js
+++ app.js
@@ -1,40 +1,53 @@
-// var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
-var request = require('request');
-var protobuf = require("protobufjs");
+// const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+const request = require('request');
+const protobuf = require("protobufjs");
+const express = require('express');
+const app = express();
 
-var requestSettings = {
+const requestSettings = {
   method: 'GET',
   url: 'http://datamine.mta.info/mta_esi.php?key=e26f09accebd90636922e54bf4de06cf&feed_id=1',
   encoding: null
 };
 
-protobuf.load("mta.proto", function(err, root) {
-  // Load the mta.proto and gtfs-realtime.proto into protobuf
-  if (err)
-    throw err;
+app.get('/', function (req, res) {
+  protobuf.load("mta.proto", function(err, root) {
+    // Load the mta.proto and gtfs-realtime.proto into protobuf
+    if (err)
+      throw err;
 
-  // Get the buffer from MTA.
-  request(requestSettings, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var FeedMessage = root.lookupType("transit_realtime.FeedMessage");
-      var message = FeedMessage.decode(body);
-      message.entity.forEach((entity) => {
-        if (entity.vehicle && entity.vehicle.trip.routeId === '1') {
-          // Vehicle Position
-          // console.log(entity.vehicle.trip['.nyctTripDescriptor']);
-        } else if(entity.tripUpdate && entity.tripUpdate.trip.routeId === '1' && entity.tripUpdate.trip['.nyctTripDescriptor'].direction === 3) {
-          // Trip Update
-          // Find the relevant stop, 124S
-          entity.tripUpdate.stopTimeUpdate.forEach((stop) => {
-            if (stop.stopId === '124S') {
-              var d = new Date();
-              var posix = d.getTime() / 1000;
-              var difference = stop.arrival.time.low - posix;
-              console.log(Math.round(difference / 60));
-            }
-          });
-        }
-      });
-    }
+    // Get the buffer from MTA.
+    request(requestSettings, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        const FeedMessage = root.lookupType("transit_realtime.FeedMessage");
+        const message = FeedMessage.decode(body);
+        const timings = [];
+        message.entity.forEach((entity) => {
+          if (entity.vehicle && entity.vehicle.trip.routeId === '1') {
+            // Vehicle Position
+            // console.log(entity.vehicle.trip['.nyctTripDescriptor']);
+          } else if(entity.tripUpdate && entity.tripUpdate.trip.routeId === '1' && entity.tripUpdate.trip['.nyctTripDescriptor'].direction === 3) {
+            // Trip Update
+            // Find the relevant stop, 124S
+            entity.tripUpdate.stopTimeUpdate.forEach((stop) => {
+              if (stop.stopId === '124S') {
+                const d = new Date();
+                const posix = d.getTime() / 1000;
+                const difference = stop.arrival.time.low - posix;
+                const minutes = Math.round(difference / 60);
+                console.log(minutes);
+                timings.push(minutes);
+              }
+            });
+          }
+        });
+
+        res.send(timings.join(', '));
+      }
+    });
   });
 });
+
+app.listen(3000);
+
+
